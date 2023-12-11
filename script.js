4f8e60e9c4027aebcba777cef8fcbd0f65db8c97
unknown <ayoolablessing54@gmail.com>
2023-12-11 02:18:47 -0800

    update

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const API = 'https://api.worldnewsapi.com/search-news?api-key=9fcf6ad69db34367a30187f60b759df3'
+const API = 'https://api.worldnewsapi.com/search-news?api-key=166fade0b0ff4c2d8408191b910b5ae2&text=sport.'
 // const API = 'https://newsdata.io/api/1/news?apikey=pub_34286f5b173f82b114e6939354fd98ff72dec&q=pegasus&language=en'
 // const API = 'http://api.mediastack.com/v1/news?access_key=e563d086f193f8da448cd2200b99ab26'
 let row = document.getElementById('row')
 fetch(API)
     .then(result => result.json())
     .then((data) => {
-        console.log(data.news)
+        // console.log(data.news)
         data.news.map((value, index) => {
             row.innerHTML += `
         
             <div class="js-style">
-                <img src="${value.image[0]}" alt="" style="width:100%;height:100px">
+                <img src="${value.image}" alt="" style="width:100%;height:200px">
                 <strong>Author: ${value.author}</strong>
                 <p><b>Title:</b> ${value.title}</p>
                 <p><b>Date:</b> ${value.publish_date}</p>
