8048259645c16d1cb86d5b57d9fd027fd0a866d4
unknown <ayoolablessing54@gmail.com>
2023-12-11 03:44:25 -0800

    update

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const API = 'https://api.worldnewsapi.com/search-news?api-key=166fade0b0ff4c2d8408191b910b5ae2&text=sport.'
-// const API = 'https://newsdata.io/api/1/news?apikey=pub_34286f5b173f82b114e6939354fd98ff72dec&q=pegasus&language=en'
-// const API = 'http://api.mediastack.com/v1/news?access_key=e563d086f193f8da448cd2200b99ab26'
-let row = document.getElementById('row')
+// const inp = document.getElementById('input')
+const row = document.getElementById('row')
+const API = `https://api.worldnewsapi.com/search-news?api-key=7ed6fd00b49849bfbc7ed1a456e371b0&text=business.`
 fetch(API)
     .then(result => result.json())
     .then((data) => {
-        // console.log(data.news)
+        console.log(data.news)
         data.news.map((value, index) => {
             row.innerHTML += `
         
@@ -18,7 +17,7 @@ fetch(API)
                 <p><b>Id:</b> ${value.id}</p>
                 <p><b>Sentiment:</b> ${value.sentiment}</p>
                 <p><b>News:</b> ${value.text.slice(0,50)} </p>
-                <a href="">Read More</a>
+                <a href="${value.url}">Read More</a>
             </div>
             `
         })
